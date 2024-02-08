12f95810596915fb67401a9e6cd149d13bebbdae
dimi <dimiendrixmmiranda@gmail.com>
2024-02-08 17:43:08 -0300

    Primeira versão da tela de login de Halloween

---
+++ script.js
@@ -0,0 +1,10 @@
+const toggle = document.querySelector('#loginSign')
+const boxContent = document.querySelector('#boxContent')
+toggle.addEventListener('change', () => {
+    if(toggle.checked){
+        console.log('checado')
+        boxContent.style.transform = 'rotateY(180deg)'
+    }else{
+        boxContent.style.transform = 'rotateY(0deg)'
+    }
+})
