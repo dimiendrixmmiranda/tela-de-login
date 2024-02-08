19b9638aadbf8def959870f90d5a0053da52d3e6
dimi <dimiendrixmmiranda@gmail.com>
2024-02-08 20:54:09 -0300

    ajustes do design na pagina

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const toggle = document.querySelector('#loginSign')
 const boxContent = document.querySelector('#boxContent')
 toggle.addEventListener('change', () => {
-    if(toggle.checked){
-        console.log('checado')
+    if (toggle.checked) {
         boxContent.style.transform = 'rotateY(180deg)'
-    }else{
+    } else {
         boxContent.style.transform = 'rotateY(0deg)'
     }
 })
